a2d1424e3d58a9388dd424e7a676143661513954
Kurt Newcomb <blackabyss25@gmail.com>
2020-07-29 10:53:45 -0700

    testing
    this is what im using to try and set up a working connection

---
+++ Test/MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using static DataLibrary.BusinessLogic.UserProcessor;
+using System.Configuration;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserModel model)
+        {
+            ViewBag.Message = "Register.cshtml";
+            if (ModelState.IsValid)
+            {
+                createUser(model.UserID, model.FirstName, model.LastName, model.EmailAddress, model.Sex, model.State);
+                return RedirectToAction("Index.cshtml");
+            }
+            return View();
+        }
+
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register.cshtml";
+            return View();
+        }
+    }
+}
---
+++ Test/MVCProject/Models/UserModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public string State { get; set; }
+        
+        
+        [Required]
+        [DisplayName("User Name")]
+        [StringLength(20, MinimumLength = 4)]
+        [DefaultValue("")]
+        public string Username { get; set; }
+        
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage ="Password too short, at least 4 characters")]
+        public string Password { get; set; }
+        
+        [Display(Name ="Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords did not match.")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+        
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address required.")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email Address")]
+        [Compare("EmailAddress", ErrorMessage ="Email didn't match.")]
+        public string EmailConfirmation { get; set; }
+
+
+    }
+}
